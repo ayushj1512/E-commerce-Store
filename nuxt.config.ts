bf8bf6fbe14330853733a85a48e3e3e799ae8308
Ayush Juneja <ayushjuneja999@gmail.com>
2025-09-05 17:25:04 +0530

    again optimized

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+   modules: [
+    '@pinia/nuxt',
+  ],
   css: ["@/assets/css/tailwind.css"],
   postcss: {
     plugins: {
